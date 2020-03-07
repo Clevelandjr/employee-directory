fc5450d60409c3c8f3b1ecd39ff1b054a94b3024
Cleveland Pitt <cleveland.pitt@gmail.com>
2020-03-07 18:58:57 -0500

    put in starter code for App.js. Imported react and home.js. created index.css

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+
+
+
+function App() {
+    return (
+      <Router>
+        <div>
+        <h1>Employee Directory</h1>
+          
+        </div>
+      </Router>
+    );
+  }
+  
+  export default App;
