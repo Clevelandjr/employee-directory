8f2b5488b2ff8f7ec45b98084551ca63f50fc375
Cleveland Pitt <cleveland.pitt@gmail.com>
2020-03-07 19:34:51 -0500

    edited home.js

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header";
+import EmployeeInfo from "../EmployeeInfo";
+
+function Home() {
+    return (
+      <div>
+        
+        <Header />
+        <EmployeeInfo />
+      </div>
+    );
+  }
+  
+  export default Home;
+  
