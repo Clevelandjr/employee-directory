9a543dc546662b18e6000b70a9f69196e4e81a06
Cleveland Pitt <cleveland.pitt@gmail.com>
2020-03-25 15:52:22 -0400

    added code to index.js in Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header";
+import EmployeeInfo from "../EmployeeInfo";
+
+function Home() {
+    return (
+      <div>
+        
+        <Header />
+        <EmployeeInfo />
+      </div>
+    );
+  }
+  
+  export default Home;
