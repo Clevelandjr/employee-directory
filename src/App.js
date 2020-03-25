abc6146056af17aa3ae7b867ddd74bc2d6c4913f
Cleveland Pitt <cleveland.pitt@gmail.com>
2020-03-25 15:37:59 -0400

    edited some link issues and renamed Header to Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+  import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/pages/Home";
-
-
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import "./App.css";
 
 function App() {
-    return (
-      <Router>
-        <div>
-        <h1>Employee Directory</h1>
-          
-        </div>
-      </Router>
-    );
-  }
-  
-  export default App;
+  document.title = "Employee Directory";
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Navbar";
+import EmployeeInfo from "../EmployeeInfo";
+
+function Home() {
+    return (
+      <div>
+        
+        <Header />
+        <EmployeeInfo />
+      </div>
+    );
+  }
+  
+  export default Home;
+  
